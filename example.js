5ca2dfb8d88b53a2dc2c80b9683a545edd67d670
vinay <vinay@vinay-VirtualBox.(none)>
2013-02-11 18:52:25 +0530

    new sub

--- example.js
+++
@@ -1,7 +0,0 @@
-exports.testSomething = function(test){
-	test.ok(true, "this assertion should pass");
-};
-exports.testSomethingElse = function(test) {
-	test.ok(true, "this assertion should fail");
-};
-
